9048694746a1bb081c6954e48fa3e21217363f56
LiamWB <liamwartnerblake@gmail.com>
2025-01-24 18:11:21 +0000

    -Add hero content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,6 @@
+import { HERO_CONTENT } from "../constants";
+import profilePic from "../assets/lwb-portfolio-heropic.png";
+
 const Hero = () => {
   return (
         <div className="border-b border-neutral-900 pb-4 lg:mb-35">
@@ -6,6 +9,7 @@ const Hero = () => {
                     <div className="flex flex-col items-center lg:items-start">
                         <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Liam Wartner Blake</h1>
                         <span className="bg-gradient-to-r from-pink-400 via-yellow-300 to-orange-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</span>
+                        <p className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</p>
                     </div>
                 </div>
             </div>
