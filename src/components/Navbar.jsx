963cbec8da0a934a403abf6c1e21f45cc6392019
LiamWB <liamwartnerblake@gmail.com>
2025-01-24 17:23:19 +0000

    -Add navbar logo, react icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import logo from "../assets/lwb-portfolio-logo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
-  return <nav className='mb-20 flex items-center justify-between py-6'>
+  return <nav className='bg-red-900 mb-20 flex items-center justify-between py-6'>
     <div className='flex flex-shrink-0 items-center'>
-        <img src="" alt="" />
+        <img src={logo} alt="logo" />
+    </div>
+    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+      <FaLinkedin />
+      <FaGithub />
+      <FaInstagram />
     </div>
   </nav>
 };
