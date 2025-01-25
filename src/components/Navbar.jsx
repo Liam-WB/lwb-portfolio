88879ab3a7d0b7f76a5e6d476c0cb0fa440f0b76
LiamWB <liamwartnerblake@gmail.com>
2025-01-25 13:23:11 +0000

    -Add links to navbar icons -Alter email text in contact section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,24 @@ import { FaInstagram } from "react-icons/fa";
 const Navbar = () => {
   return <nav className='mb-20 flex items-center justify-between py-6'>
     <div className='flex flex-shrink-0 items-center'>
+      <a href="#" onClick={(e) => {
+            e.preventDefault();
+            window.location.reload();
+        }}
+      >
         <img src={logo} alt="logo" />
+      </a>
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-      <FaLinkedin />
-      <FaGithub />
-      <FaInstagram />
+      <a href="https://www.linkedin.com/in/liam-blake-985239225/">
+        <FaLinkedin />
+      </a>
+      <a href="https://github.com/Liam-WB">
+        <FaGithub />
+      </a>
+      <a href="https://www.instagram.com/liam_blake001/">
+        <FaInstagram />
+      </a>
     </div>
   </nav>
 };
